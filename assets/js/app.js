3660970c6437f12eb58d13ec0a665c270be848b3
Witold Szrama <witold.szrama@programa.pl>
2020-05-28 17:48:35 +0200

    Fix code format

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,8 +5,6 @@ require('bootstrap');
 require('@fortawesome/fontawesome-free/css/all.min.css');
 require('@fortawesome/fontawesome-free/js/all.js');
 
-
-
 $(document).ready(function() {
 
         let tableBody = $('tbody');
