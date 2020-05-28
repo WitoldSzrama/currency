9c898e2890c94a3641606e025c975d2eafe099a8
Witold Szrama <witold.szrama@programa.pl>
2020-05-28 17:20:58 +0200

    Create currency project

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,100 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
+import '../css/app.scss';
+const $ = require('jquery');
 
-// any CSS you import will output into a single css file (app.css in this case)
-import '../css/app.css';
+require('bootstrap');
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
 
-// Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-// import $ from 'jquery';
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+
+$(document).ready(function() {
+
+        let tableBody = $('tbody');
+        let date = new Date();
+        let yesterday = new Date(Date.now() - 86400000); 
+        let yesterdayString = yesterday.toISOString().substring(0,10);
+    
+    let ajaxCall = function() {
+        $('body').css('cursor', 'wait');
+        let yesterdayAjax = $.ajax({
+            url:'https://api.nbp.pl/api/exchangerates/tables/a/' + yesterdayString + '/?format=json',
+            method:'GET',
+            async: false,
+            success: function(data) {
+                return data;
+            },
+        })
+        let todayAjax = $.ajax({
+            url:'https://api.nbp.pl/api/exchangerates/tables/a/today/?format=json',
+            method: 'GET',
+            async: false,
+            success: function(data) {
+                return data;
+            }
+        })
+        let data = createMainData(yesterdayAjax.responseJSON[0].rates, todayAjax.responseJSON[0].rates)
+        
+        
+        $(tableBody).empty();
+        $.each(data, function(index, value) {
+            let changeColor = (value.currencyDiff > 0) ? 'text-success' : 'text-danger';
+            $(tableBody).append(`
+                <tr>
+                    <td class="upperCase">${value.currency}</td>
+                    <td>${value.code}</td>
+                    <td>${value.mid}</td>
+                    <td class="${changeColor}">${value.percentChange.toFixed(2)} %</td>
+                    <td class="${changeColor}">${value.currencyDiff.toFixed(2)} zl</td>
+                </tr>
+            `)
+        })
+        $('body').css('cursor', 'default');
+    }
+
+    ajaxCall();
+
+    $('#refreshResults').on('click', function() {
+        removeArrow();
+        $('tbody').empty();
+        ajaxCall();
+    })
+     $('th').click(function(){
+        var table = $(this).parents('table').eq(0)
+        var rows = table.find('tr:gt(0)').toArray().sort(comparer($(this).index()))
+        this.asc = !this.asc
+        removeArrow();
+        if (!this.asc){
+            rows = rows.reverse()
+            $(this).find('span').html(' <i class="fas fa-arrow-up"></i>');
+        } else {
+            $(this).find('span').html(' <i class="fas fa-arrow-down"></i>');
+        }
+        for (var i = 0; i < rows.length; i++){table.append(rows[i])}
+    })
+    function comparer(index) {
+        return function(a, b) {
+            var valA = getCellValue(a, index), valB = getCellValue(b, index)
+            return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.toString().localeCompare(valB)
+        }
+    }
+    function getCellValue(row, index){ return $(row).children('td').eq(index).text() }
+
+    let removeArrow =function() {
+        $('span').html('');
+    }
+});
+
+let createMainData = function(yesterdayData, todaysData) {
+    let data = [];
+    $.each(todaysData, function(index, value) {
+        data[index] = {
+            'currency': value.currency,
+            'code': value.code,
+            'mid': value.mid,
+            'percentChange': value.mid * 100 / yesterdayData[index].mid,
+            'currencyDiff': value.mid - yesterdayData[index].mid,
+        };
+    })
+
+   return data;
+}
